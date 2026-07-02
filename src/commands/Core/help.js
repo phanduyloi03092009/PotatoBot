f2083f9b17f2d01185746640b1a270420fe3671b
Potato <162554599+phanduyloi03092009@users.noreply.github.com>
2026-07-02 22:46:03 +0700

    Update help.js

--- src/commands/Core/help.js
+++ src/commands/Core/help.js
@@ -119,7 +119,7 @@ export async function createInitialHelpMenu(client) {
 
     const supportButton = new ButtonBuilder()
         .setLabel("Support Server")
-        .setURL("https://discord.gg/QnWNz2dKCE")
+        .setURL("https://discord.gg/3jtM5nNTBn")
         .setStyle(ButtonStyle.Link);
 
     const selectRow = createSelectMenu(
